55559963c5a52ae2224695a8184756e62461594d
TS22082 <ts22082@gmail.com>
2021-07-26 13:39:15 -0700

    set up react-router-dom

--- example/src/App.js
+++ example/src/App.js
@@ -3,8 +3,29 @@ import React from 'react'
 import { Thing } from 'bit-ui-wise'
 import 'bit-ui-wise/dist/index.css'
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
 const App = () => {
-  return <Thing text='Create React Library Example 😄' />
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <h1>This is the UI library</h1>
+          </Route>
+          <Route path='/team1'>
+            <h1>im for team 1</h1>
+          </Route>
+          <Route path='/team2'>
+            <h1>im for team 2</h1>
+          </Route>
+          <Route path='/team3'>
+            <h1>im for team 3</h1>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  )
 }
 
 export default App
