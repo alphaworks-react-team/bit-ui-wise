2c673f72a408871a5d382401b583a0e2b130d4d5
MarkGee45 <moel55.mg@gmail.com>
2021-07-26 17:29:33 -0700

    Add: created multiple materialized buttons

--- example/src/Pages/Team2.js
+++ example/src/Pages/Team2.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import {Button} from 'bit-ui-wise'
 
 const Team2 = () => {
   return (
     <div>
       <h1>Im for team 2</h1>
+      <Button variant="primary">Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="delete">Delete</Button>
+      <Button variant="warning">Warning</Button>
+      <Button variant="send">Send</Button>
+      <Button variant="upload">Upload</Button>
+      <Button variant="save">Save</Button>
+      <Button variant="submit">Submit</Button>
+      {/* <Button variant="shuffle">Delete</Button>
+      <Button variant="play">Delete</Button>
+      <Button variant="forward">Delete</Button>
+      <Button variant="rewind">Delete</Button> */}
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './styles.module.css'
 import Thing from './Thing/Thing'
+import Button from './Button/Button'
 
-export { Thing }
+export { Thing, Button }
