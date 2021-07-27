02c8ea0b75a190841d21e7607f1867e2690d7677
Akiko Green <agreen@alphaworks.tech>
2021-07-27 02:16:58 +0000

    PULLED from dev

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import Card from '../Card/Card'
 const Button = (props) => {
 //   const styles = {
 //     // primary: {
--- src/Card/Card.js
+++ src/Card/Card.js
@@ -3,6 +3,7 @@ import React from 'react';
 // import PropTypes from 'prop-types'
 import './card.css';
 
+
 const Card = (props) => {
 
   const styles = {
