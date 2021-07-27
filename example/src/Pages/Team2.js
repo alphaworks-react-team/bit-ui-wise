217e909dded6bcfca8f5bebc7b7e18273fd9e82f
angelz510 <angelzrod4@gmail.com>
2021-07-27 10:29:43 -0700

    pulled team-2 setup

--- example/src/Pages/Team2.js
+++ example/src/Pages/Team2.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Button } from 'bit-ui-wise';
 
 const Team2 = () => {
   return (
     <div>
       <h1>Im for team 2</h1>
+      <Button>This is a button</Button>
     </div>
   )
 }
