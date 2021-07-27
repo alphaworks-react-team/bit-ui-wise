8a73e77cfbf6606b25d870761053448b10fed597
Franco <phanfranco@yahoo.com>
2021-07-27 12:53:10 -0700

    UPDATE: props.children and styling were doine.

--- example/src/Pages/Team3.js
+++ example/src/Pages/Team3.js
@@ -1,33 +1,34 @@
 import React from 'react'
-import {Card} from 'bit-ui-wise'
+import { Card } from 'bit-ui-wise'
 import PropTypes from 'prop-types'
 
 const Team3 = () => {
   const styles = {
-    display: "flex",
-    justifyContent: "center",
-    alignItems:"center",
-    height:"100vh",
-    backgroundColor:"#9e9e9e"
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    backgroundColor: '#9e9e9e'
   }
 
-  
   return (
     <div style={styles}>
       <Card
         title='hello'
-        text='hella lorem'
+        subTitle='subtitle'
         img={'https://picsum.photos/400/600'}
         btnOneText={'hello'}
         btnTwoText={'hello'}
-        width={'auto'}
-      />
+        width='300px'
+        height='500px'
+      >
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur
+        laboriosam explicabo expedita illum voluptates magnam! Quasi harum
+        explicabo sed voluptatum eum tenetur, rerum ullam, inventore neque
+        labore voluptates distinctio. Est.
+      </Card>
     </div>
   )
 }
-
-
-
-
 
 export default Team3
--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,75 +1,91 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
+import React from 'react'
 // import PropTypes from 'prop-types'
-import './card.css';
+import './card.css'
 
 const Card = (props) => {
-
   const styles = {
     main: {
-      width: '200px',
-      // height: "100px",
-      backgroundColor: 'white'
+      width: `${props.width || '200px'}`,
+      maxHeight: '100%',
+      // height: `${props.height}`,
+      height: `${props.height}`,
+      backgroundColor: 'white',
+      boxShadow: '3px 3px 5px black'
     },
     img: {
       backgroundImage: `url(${props.img})`,
       backgroundSize: 'cover',
       backgroundPosition: 'center',
       backgroundRepeat: 'no-repeat',
-      height: '150px'
+      height: '50%'
     },
     textBody: {
-      height: '100%',
-      width: '100%'
+      height: '50%',
+      width: 'auto',
+      margin: '0 15px 0 15px',
+      display: 'flex',
+      flexFlow: 'column',
+      justifyContent: 'space-around',
+      alignItems: 'flex-start'
     },
     title: {
-      fontSize: `${props.titleSize || "20px"}`, // possible prop
-      fontWeight: 'bold',
-      margin: '10px' // possible prop
-    },
-    text: {
-      margin: '10px'
+      fontSize: `${props.titleSize || '20px'}`, // possible prop
+      fontWeight: 'bold'
+      // margin: '10px' // possible prop
     },
     button: {
       // width: '40%',
-      width: props.width,
+      width: 'auto',
       height: '20px',
       border: 'none',
       backgroundColor: 'white',
-      cursor: 'pointer',
-      margin: '10px',
-      padding: "0"
+      cursor: 'pointer'
+      // margin: '10px',
+      // paddingRight: '0'
+    },
+    buttonGroup: {
+      width: '100%',
+      display: 'flex',
+      flexFlow: 'row',
+      justifyContent: 'flex-start'
     }
   }
-  
-  
+
   return (
-    
     <div className='main' style={styles.main}>
-      {props.img ? 
-      ( <div className='img' style={styles.img}>{}</div>) : null}
+      {props.img ? (
+        <div className='img' style={styles.img}>
+          {}
+        </div>
+      ) : null}
       <div className='textBody' style={styles.textBody}>
-        {props.title ? (<div className='title' style={styles.title}>
-          {props.title}
-        </div>) : null}
-        {props.text ? (<div className='text' style={styles.text}>
-          {props.text}
-        </div>) : null}
-        {props.btnOneText ? (
-          <button className='cardButton1' style={styles.button}>
-            {props.btnOneText}
-          </button>
-        ) : null}
-        {props.btnTwoText ? (
-          <button className='cardButton2' style={styles.button}>
-            {props.btnTwoText}
-          </button>
-        ) : null}
-        
+        <div className='header'>
+          {props.title ? (
+            <div className='title' style={styles.title}>
+              {props.title}
+            </div>
+          ) : null}
+          {props.subTitle ? (
+            <div className='subTitle'>{props.subTitle}</div>
+          ) : null}
+        </div>
+        {props.children ? <div className='text'>{props.children}</div> : null}
+        <div className='buttonGroup' style={styles.buttonGroup}>
+          {props.btnOneText ? (
+            <button className='cardButton' style={styles.button}>
+              {props.btnOneText}
+            </button>
+          ) : null}
+          {props.btnTwoText ? (
+            <button className='cardButton' style={styles.button}>
+              {props.btnTwoText}
+            </button>
+          ) : null}
+        </div>
       </div>
     </div>
   )
 }
-
 
 export default Card
