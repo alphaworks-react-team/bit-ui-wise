0ba3b95e11d4b447c78a437138261e357dd2db85
andrewjspivey <12spivey@gmail.com>
2021-07-27 13:01:28 -0700

    ADD: adds size prop and conditionals to Button

--- example/src/Pages/Team2.js
+++ example/src/Pages/Team2.js
@@ -5,14 +5,14 @@ const Team2 = () => {
   return (
     <div>
       <h1>Im for team 2</h1>
-      <Button variant="primary">Primary</Button>
-      <Button variant="secondary">Secondary</Button>
-      <Button variant="delete">Delete</Button>
+      <Button size="large" variant="primary">Primary</Button>
+      <Button size="medium" variant="secondary">Secondary</Button>
+      <Button size="small" variant="delete">Delete</Button>
       <Button variant="warning">Warning</Button>
-      <Button variant="send">Send</Button>
+      <Button size="large" variant="send">Send</Button>
       <Button variant="upload">Upload</Button>
-      <Button variant="save">Save</Button>
-      <Button variant="submit">Submit</Button>
+      <Button size="medium" variant="save">Save</Button>
+      <Button size="small" variant="submit">Submit</Button>
       {/* <Button variant="shuffle">Delete</Button>
       <Button variant="play">Delete</Button>
       <Button variant="forward">Delete</Button>
