67a3faee24b71684d7641e0b61c3b147c6fac244
jerniceduncan <jerniceduncan@icloud.com>
2021-08-02 14:16:03 -0700

    update team1 JS

--- example/src/Pages/Team1.js
+++ example/src/Pages/Team1.js
@@ -2,23 +2,11 @@ import React from 'react'
 import { Typography, TextField, practice } from 'bit-ui-wise'
 
 const Team1 = () => {
-  const styles = {
-    display: 'flex',
-    width: '100%',
-    minHeight: '10vh',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'lightgrey'
-  }
   return (
     <div>
-      <nav style={styles}>
-        <Typography variant='subtitle'>I Got This</Typography>
-      </nav>
-
-      <Typography variant='sub-header'>This is some text</Typography>
-      <Typography variant='subtitle'>This is some text</Typography>
-      <Typography variant='body'>This is some text</Typography>
+      <Typography variant='large'>This is some text</Typography>
+      <Typography variant='medium'>This is some text</Typography>
+      <Typography variant='small'>This is some text</Typography>
       <Typography>This is some text</Typography>
 
       <TextField />
