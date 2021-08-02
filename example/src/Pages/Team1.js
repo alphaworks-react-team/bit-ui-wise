2bf14e18c6b870a27b984c7623c7a5397234b8d1
Ariel Agress <ari@ariels-mac-mini.attlocal.net>
2021-08-02 11:12:33 -0700

    Aris edit

--- example/src/Pages/Team1.js
+++ example/src/Pages/Team1.js
@@ -4,17 +4,23 @@ import { Typography, TextField, practice } from 'bit-ui-wise'
 const Team1 = () => {
   return (
     <div>
-      <Typography variant='header'>This is some text</Typography>
-      <Typography variant='sub-header'>This is some text</Typography>
-      <Typography variant='subtitle'>This is some text</Typography>
-      <Typography variant='body'>This is some text</Typography>
-      <Typography>This is some text</Typography>
+      <Typography variant='large'>This is some text</Typography>
+      <Typography variant='medium'>This is some text</Typography>
+      <Typography variant='small'>This is some text</Typography>
+      <Typography
+        variant='small'
+        fontSize='1em'
+        fontFamily='Times New Roman'
+        fontColor='orange'
+        fontWeight='700'
+      >
+        This is some text
+      </Typography>
 
       <TextField />
       <TextField placeholder={'im a placeholder'} />
       <TextField inputStyle={'outline'} value={'im a value'} width={'25%'} />
       <TextField inputStyle={'filled'} value={'im a value'} width={'25%'} />
-
     </div>
   )
 }
